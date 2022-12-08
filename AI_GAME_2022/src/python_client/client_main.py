40d84ed2b614e2bfcfbe4a950639a6d60d1b4527
parisa <prisashahbi@gmail.com>
2022-12-08 19:52:48 +0330

    initial commit

---
+++ AI_GAME_2022/src/python_client/client_main.py
@@ -0,0 +1,15 @@
+import random
+from base import BaseAgent, Action
+
+
+class Agent(BaseAgent):
+    # actions = [Action.RIGHT] + [Action.TELEPORT] * 100
+    def do_turn(self) -> Action:
+        return random.choice(
+            [Action.UP, Action.DOWN, Action.LEFT, Action.RIGHT, Action.DOWN_RIGHT, Action.DOWN_LEFT, Action.UP_LEFT,
+             Action.UP_RIGHT, Action.NOOP])
+
+
+if __name__ == '__main__':
+    data = Agent().play()
+    print("FINISH : ", data)
